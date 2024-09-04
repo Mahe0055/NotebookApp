bb899a94ecef489c03140d883ff0273ba51b4d89
Mahe0055 <mahe00555@stud.kea.dk>
2024-09-04 11:24:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [note, setNote] = useState("");
+  function buttonHandler() {
+    alert("Din note " + note + " er gemt");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.noteText}>
+        Velkommen! Her kan du skrive og gemme dine noter
+      </Text>
+      <TextInput style={styles.input} onChangeText={(txt) => setNote(txt)} />
+      <View>
+        <TouchableOpacity style={styles.button} onPress={buttonHandler}>
+          <Text style={styles.buttonText}>Gem note</Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "column",
+  },
+  noteText: {
+    fontSize: 30,
+    padding: 20,
+    fontWeight: "bold",
+    marginTop: 80,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#ffbe30",
+    padding: 15,
+    borderRadius: 5,
+    marginBottom: 80,
+  },
+  buttonText: {
+    color: "#000000",
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    borderColor: "black", // Sort kant
+    borderWidth: 2, // Tykkelse af kanten
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: "30%",
+    borderRadius: 5, // Afrunding af hjørner
+    backgroundColor: "#f5f5f5", // grå baggrundsfarve
   },
 });
